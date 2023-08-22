1360694a07d383659dd25c28e0827fa00a52a02f
Kgaogelo Phooko <1843244@students.wits.ac.za>
2023-08-22 08:25:01 -0700

    printf errors

--- printf0.c
+++ printf0.c
@@ -1,68 +1,71 @@
 #include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <string.h>
 
 /**
- * _printf - function that produces output according to a format
- * @format: character string
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: the number of printed characters
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _printf - nction that produces output according to a format
+ * @format: the format string
+ *
+ * Return:  the number of characters printed
+ */
+
 int _printf(const char *format, ...)
 {
-	va_list arguments;
-	int char_print = 0;
+	va_list args;
+	int char_print;
 
-	va_start(arguments, format);
+	va_start(args, format);
 	char_print = 0;
 
-	if (format == NULL)
-	return (-1);
 	while (*format != '\0')
 	{
-	int c;
+		if (*format == '%')
+		{
+			format++;
+		if (*format == '\0')
+			break;
+		if (*format == 'c')
+		{
+                	int c = va_arg(args, int);
+                	_putchar(c);
+                	char_print++;
+		}
+		else if (*format == 's')
+		{
+			const char *string = va_arg(args, const char *);
 
-	if (*format == '%')
-	{
-	format++;
-	if (*format == '\0')
-		break;
-	if (*format == 'c')
-	{
-	c = va_arg(arguments, int);
-
-	putchar(c);
-	char_print++;
-	}
-	else if (*format == 's')
-	{
-	const char *string;
-
-	string = va_arg(arguments, const char *);
-
-	while (*string != '\0')
-	{
-	putchar(*string);
-	string++;
-	char_print++;
-	}
-	}
-	else if (*format == '%')
-	{
-	putchar('%');
-	char_print++;
-	}
-	}
-	else
-	{
-	putchar(*format);
-	char_print++;
-	}
-	format++;
+			while (*string != '\0')
+			{
+				_putchar(*string);
+				string++;
+				char_print++;
+			}
+		}
+		else if (*format == '%')
+		{
+			putchar('%');
+			char_print++;
+		}
+		}
+		else
+		{
+		putchar(*format);
+		char_print++;
+		}
+		format++;
 	}
 
-	va_end(arguments);
+	va_end(args);
 
 	return (char_print);
 }
