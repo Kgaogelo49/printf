5b1aa36095611aefccc901695ca67cd8b547309e
Kgaogelo Phooko <1843244@students.wits.ac.za>
2023-08-23 07:00:24 -0700

    fixing printf

--- printf0.c
+++ printf0.c
@@ -10,7 +10,7 @@ int _printf(const char *format, ...)
 	va_list arguments;
 	int char_print = 0;
 
-	if (format == NULL)
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 	return (-1);
 
 	va_start(arguments, format);
